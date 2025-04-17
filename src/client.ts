2534300df8483696225b1033d6456421a7a7e3ce
Alberto Rodriguez <alberto@filmhub.com>
2025-04-17 03:47:00 +0200

    PATCH: Fixed the queryBulk function so that it can receive an array of strings and then validate the ips

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { IPAddress, IPInfo } from './types';
+import { IPInfo, IPAddress } from './types';
 
 const BASE_URL = 'https://api.ipquery.io/';
 
@@ -7,6 +7,15 @@ function isValidIP(ip: string): boolean {
   const ipv4 = /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/;
   const ipv6 = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
   return ipv4.test(ip) || ipv6.test(ip);
+}
+
+function asIPAddresses(ips: string[]): IPAddress[] {
+  return ips.map(ip => {
+    if (!isValidIP(ip)) {
+      throw new Error(`Invalid IP address: ${ip}`);
+    }
+    return ip as IPAddress;
+  });
 }
 
 export async function queryIP(ip: IPAddress): Promise<IPInfo> {
@@ -22,13 +31,9 @@ export async function queryOwnIP(): Promise<string> {
   return response.data;
 }
 
-export async function queryBulk(ips: IPAddress[]): Promise<IPInfo[]> {
-  for (const ip of ips) {
-    if (!isValidIP(ip)) {
-      throw new Error(`Invalid IP address: ${ip}`);
-    }
-  }
-  const ipList = ips.join(',');
+export async function queryBulk(ips: string[]): Promise<IPInfo[]> {
+  const validatedIps = asIPAddresses(ips);
+  const ipList = validatedIps.join(',');
   const response = await axios.get(`${BASE_URL}${ipList}`);
   return response.data;
 }
